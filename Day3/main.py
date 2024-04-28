6e9be5235bc1193c62fe08f74572108f68f7e0b5
Lavesh Paryani <laveshparyani01@gmail.com>
2024-04-29 01:34:18 +0530

    Added Day 3 with main.py and dependencies

---
+++ Day3/main.py
@@ -0,0 +1,31 @@
+#pip install pandas (Library used for data analysis)
+
+#python (REPL)
+#>>>import pandas (No error because already installed)
+#>>>import sklearn (Error, because not installed and also not a built-in module.)
+#>>>exit()
+
+#pip install scikit-learn
+#python
+#>>>import sklearn (No error because already installed)
+
+import pandas 
+#external module, hence installation is necessary before using
+
+import hashlib
+#built-in module, hence installation is not necessary before using
+
+import tensorflow
+#external module, hence installation is necessary before using
+
+print("Hi")
+
+
+# import pandas  # This is an example of external module
+# import hashlib  # This is an example of built in module
+
+# print("Hi")
+
+# # Dont worry about how to use these modules just yet!
+# pandas.read_csv("C:\\Users\\anony\\OneDrive\\Desktop\\Learning_Python\\Day3\\one.csv")
+# m = hashlib.sha256()
