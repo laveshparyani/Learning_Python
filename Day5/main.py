3b7b342518db414ce130421a61f02999644bc497
Lavesh Paryani <laveshparyani01@gmail.com>
2024-06-11 21:20:30 +0530

    Day5 updated.

--- Day5/main.py
+++ Day5/main.py
@@ -1,4 +1,22 @@
+#This is an example of single-line comment
 print("Hey, I am Lavesh!")
 
-#Don't remove this line
-print("Hey, I am Lavesh!\nWho are you")
+print("Hey, I am Lavesh!\nWho are you") #This is an example of newline character "\n"
+
+#Multi-line comment using triple double-quotes👇🏻
+
+"""This is an example of multi-line comment
+and below written is if-else statement."""
+
+p=7
+if(p>5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+    
+'''End of if-else statement.'''
+#Multi-line comment using triple single-quotes☝🏻
+
+#Example of Escape Sequence character👇🏻
+#print("This will not "execute"")
+print("This will \"execute\", as here escape sequence character is used.")
